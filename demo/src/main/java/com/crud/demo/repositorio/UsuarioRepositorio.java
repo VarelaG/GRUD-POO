1e5a960ab266a343569c3f991783d12298f1d1ad
Varela <lancelo-1@hotmail.com>
2023-11-27 18:20:50 -0300

    usuarioRegristro,rol,usuario,usuarioRepositorio

---
+++ demo/src/main/java/com/crud/demo/repositorio/UsuarioRepositorio.java
@@ -0,0 +1,12 @@
+package com.crud.demo.repositorio;
+
+import com.crud.demo.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+
+    public Usuario findByEmail(String email);
+
+}
