f0ba048ed293b908539758b644e02d1f6c6f8fcf
Varela <lancelo-1@hotmail.com>
2023-11-28 19:45:41 -0300

    SecurityConfiguration arreglada --> Referencia circular de propperties

--- demo/src/main/java/com/crud/demo/seguridad/SecurityConfiguration.java
+++ demo/src/main/java/com/crud/demo/seguridad/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.crud.demo.seguridad;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +11,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import com.crud.demo.servicio.UsuarioServicio;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration {
 
     @Autowired
     private UsuarioServicio usuarioServicio;
@@ -33,29 +34,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         return auth;
     }
 
-    @Override
+
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
     }
 
-    @Override
+
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(
+        http
+                .userDetailsService(usuarioServicio)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
                         "/registro**",
                         "/js/**",
                         "/css/**",
                         "/img/**").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout")
-                .permitAll();
+                )
+                        .formLogin((form) -> form.permitAll())
+                        .logout((logout) -> logout.permitAll());
+
     }
 }
