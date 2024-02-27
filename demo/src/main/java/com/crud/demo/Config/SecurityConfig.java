387cae445e5d024a3350040694104626159e549c
facundosantacruz <facusantacruz@hotmail.com>
2024-02-27 19:08:52 -0300

    Lo que presentamos en diciembre, habia quedado sin commitear

---
+++ demo/src/main/java/com/crud/demo/Config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.crud.demo.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/auth/**", "/eventos").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin((withDefaults()))
+                .build();
+    }
+
+}
